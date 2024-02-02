1d2f98bab4547640f172e868793ecf2087f742f0
balen nouri <balennouri@gmail.com>
2024-02-02 20:25:25 +0000

    Feat: change color on footer

--- eliteplayer/urls.py
+++ eliteplayer/urls.py
@@ -22,5 +22,5 @@ from booking.views import my_booking
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('booking', my_booking, name='booking'),
-    path('home/', my_home, name='home'),
+    path('', my_home, name='home'),
 ]
